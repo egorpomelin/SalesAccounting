bc4e9275dabc862e53062e5a6e4d5af84fd337c8
egor <egorpomelin@gmail.com>
2022-07-25 21:54:56 +0300

    реализовано подключение к базе данных

---
+++ Infrastructure/ConnectionDB.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using SalesAccounting.Interfece;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SalesAccounting.Infrastructure
+{
+    internal class ConnectionDB : IConnectionDB
+    {
+        MySqlConnection connection = new MySqlConnection($"server={Properties.Settings.Default.serverAddress};username={Properties.Settings.Default.user};password={Properties.Settings.Default.password};database={Properties.Settings.Default.nameDB}");
+        
+        public void closeCOnnection(string stringConnection)
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Нет подключения");
+            }
+        }
+
+        public void openCOnnection(string stringConnection)
+        {
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Нет подключения");
+            }
+        }
+
+        public MySqlConnection returnConnection()
+        {
+            try
+            {
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Нет подключения");
+
+                return null;
+            }
+        }
+    }
+}
--- ViewModel/SalesAccountingWindowViewModel.cs
+++ ViewModel/SalesAccountingWindowViewModel.cs
@@ -1,14 +1,21 @@
-﻿using SalesAccounting.ViewModel.Base;
+﻿using SalesAccounting.Infrastructure;
+using SalesAccounting.ViewModel.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SalesAccounting.ViewModel
 {
     internal class SalesAccountingWindowViewModel : ViewModelBase
     {
+
+        public SalesAccountingWindowViewModel()
+        {
+            AddProductCheque = new command(OnAddProductChequeExecuted, CanAddProductChequeExecuted);
+        }
 
         string title = "Регистрация продаж";
         public string Title
@@ -16,5 +23,25 @@ namespace SalesAccounting.ViewModel
             get { return title; }
             set { Set(ref title, value); }
         }
+
+        int barcode = 0;
+        public int Barcode
+        {
+            get { return barcode; }
+            set { Set(ref barcode, value); }
+        }
+
+        public ConnectionDB DB = new ConnectionDB();
+
+        public ICommand AddProductCheque { get; }//добавление товара в чек
+
+        private void OnAddProductChequeExecuted(object p)
+        {
+            
+        }
+
+        private bool CanAddProductChequeExecuted(object p) => true;
+
+
     }
 }
