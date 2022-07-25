a158008b2d5f02131a7379d4889a4a2948917326
egor <egorpomelin@gmail.com>
2022-07-25 18:46:37 +0300

    Разметка главного окна

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -1,9 +1,13 @@
-﻿using RabotaWpf.ViewModel.Base;
+﻿using GalaSoft.MvvmLight.Command;
+using RabotaWpf.ViewModel.Base;
+using SalesAccounting.View.ViewPage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SalesAccounting.ViewModel
 {
@@ -15,6 +19,27 @@ namespace SalesAccounting.ViewModel
         {
             get { return title; }
             set { Set(ref title, value); }
+        }
+
+
+        private Page indexPage = new IndexPage();
+        private Page settingsPage = new SettingsPage();
+        private Page _CurPage = new IndexPage();
+        
+        public Page CurPage
+        {
+            get => _CurPage;
+            set => Set(ref _CurPage, value);
+        }
+
+        public ICommand OpenIndexPage
+        {
+            get { return new RelayCommand(() => CurPage = indexPage); }
+        }
+
+        public ICommand OpenSettingsPage
+        {
+            get { return new RelayCommand(() => CurPage = settingsPage); }
         }
     }
 }
