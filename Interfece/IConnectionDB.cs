a1bab397d3085827dbadb90c006167cc1542dfa7
egor <egorpomelin@gmail.com>
2022-07-25 18:07:52 +0300

    Добавьте файлы проекта.

---
+++ Interfece/IConnectionDB.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesAccounting.Interfece
+{
+    internal interface IConnectionDB
+    {
+        public void openCOnnection(string stringConnection); //открывает соединение 
+        public void closeCOnnection(string stringConnection);//закрывает соединение
+        public MySqlConnection returnConnection();//возвращает соединение
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RabotaWpf.ViewModel.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesAccounting.ViewModel
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+
+        string title = "Sales Accounting";
+        public string Title
+        {
+            get { return title; }
+            set { Set(ref title, value); }
+        }
+    }
+}
